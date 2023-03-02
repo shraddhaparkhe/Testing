df4822d916f01b63a4a197e0b9004d6fd271dc49
github_username <email_address>
2023-03-02 07:23:21 +0530

    my

---
+++ Test.com.test/src/main/java/ja.java
@@ -0,0 +1,11 @@
+
+public class ja {
+	int a = 5;
+      int b =10;
+       int c= a+b;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("c");
+	}
+
+}
